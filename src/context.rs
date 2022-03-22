6392e7db02a11e669a798d803d00a72591996f70
Andreas Wass <wass.andreas@gmail.com>
2022-03-22 12:31:56 +0100

    Refactor some stuff

--- src/context.rs
+++ src/context.rs
@@ -3,12 +3,28 @@ use std::cell::RefCell;
 use std::collections::VecDeque;
 use std::thread_local;
 
+pub struct ReportedError
+{
+    pub id: u32,
+    pub type_id: TypeId,
+    pub value: *mut (),
+}
+
+/// The result of `ErrorHandlingContext.try_set_error`
+///
+/// The handling of the value will differ based on the returned value.
+pub enum TrySetErrorResult
+{
+    /// The error was unhandled
+    NotHandled,
+    /// The error was handled, the caller must `forget` the actual error
+    NeedForget,
+    /// The error was handled, the caller must `drop` the actual error
+    NeedDrop,
+}
+
 pub trait ErrorHandlingContext: 'static
 {
-    /// Checks if the handling context holds an active error already.
-    ///
-    /// Each context should only hold 1 active error per context.
-    fn holds_active_error(&self) -> bool;
     /// Try to store the error into a handling context
     ///
     /// If this error handling context can handle the type described by `type_id` then it should
@@ -16,39 +32,48 @@ pub trait ErrorHandlingContext: 'static
     ///
     /// ## Safety
     ///
-    ///   * The `TypeId` of the actual type of the pointer that `value` points to must match
-    ///     `type_id`.
+    ///   * The `TypeId` of the actual type of the pointer that `error.value` points to must match
+    ///     `error.type_id`.
     ///   * If this function returns true, the caller must ensure to `forget` the original value
     ///     since it is effectively moved to some other location.
-    unsafe fn try_set_error(&mut self, type_id: TypeId, value: *mut ()) -> bool;
+    unsafe fn try_set_error(&mut self, error: &ReportedError) -> TrySetErrorResult;
 }
 
-impl<T: 'static> ErrorHandlingContext for Option<T>
+impl<T: crate::Error> ErrorHandlingContext for Option<(u32, T)>
 {
-    fn holds_active_error(&self) -> bool {
-        self.is_some()
-    }
-    unsafe fn try_set_error(&mut self, type_id: TypeId, value: *mut ()) -> bool {
-        if TypeId::of::<T>() == type_id {
-            *self = Some((value as *mut T).read());
-            true
+    unsafe fn try_set_error(&mut self, error: &ReportedError) -> TrySetErrorResult {
+        if TypeId::of::<T>() == error.type_id {
+            *self = Some((error.id, (error.value as *mut T).read()));
+            TrySetErrorResult::NeedForget
         }
         else {
-            false
+            TrySetErrorResult::NotHandled
         }
+    }
+}
+
+struct CatchAllContext
+{
+    pub inner: Option<(u32, TypeId)>
+}
+
+impl ErrorHandlingContext for CatchAllContext {
+    unsafe fn try_set_error(&mut self, error: &ReportedError) -> TrySetErrorResult {
+        self.inner = Some((error.id, error.type_id));
+        TrySetErrorResult::NeedDrop
     }
 }
 
 struct HandlingScopes
 {
-    error_count: u32,
+    error_id: u32,
     scopes: VecDeque<HandlingScope>
 }
 
 impl HandlingScopes {
     fn new() -> Self {
         Self {
-            error_count: 0,
+            error_id: 0,
             scopes: VecDeque::new()
         }
     }
@@ -75,12 +100,6 @@ thread_local! {
     static CONTEXTS: RefCell<HandlingScopes> = RefCell::new(HandlingScopes::new());
 }
 
-pub(crate) fn reset() {
-    CONTEXTS.with(|c| {
-        *c.borrow_mut() = HandlingScopes::new();
-    });
-}
-
 pub unsafe fn push_handling_context(handling_context: &mut dyn ErrorHandlingContext) {
     CONTEXTS.with(|contexts| {
         let mut ctx = contexts.borrow_mut();
@@ -95,37 +114,39 @@ pub unsafe fn pop_handling_context() {
     })
 }
 
-pub fn push_error<E: 'static>(mut err: E) {
+pub struct PopHandlingContextGuard;
+
+impl Drop for PopHandlingContextGuard {
+    fn drop(&mut self) {
+        unsafe { pop_handling_context() };
+    }
+}
+
+pub fn push_error<E: crate::Error>(mut err: E) -> u32 {
     CONTEXTS.with(move |contexts| {
         let mut ctx = contexts.borrow_mut();
-        ctx.error_count += 1;
-        let err_type = TypeId::of::<E>();
-        let err_ptr = &mut err as *mut _ as *mut ();
+        ctx.error_id = ctx.error_id.wrapping_add(1);
+        let reported_error = ReportedError {
+            id: ctx.error_id,
+            type_id: TypeId::of::<E>(),
+            value: &mut err as *mut _ as *mut (),
+        };
+
         for x in ctx.scopes.iter_mut() {
             let context = unsafe { x.context() };
-            if unsafe { context.try_set_error(err_type, err_ptr) } {
+            match unsafe { context.try_set_error(&reported_error) } {
                 // SAFETY: We must ensure to forget err if we end up here!
-                std::mem::forget(err);
-                return;
+                TrySetErrorResult::NeedForget => {
+                    std::mem::forget(err);
+                    return reported_error.id;
+                },
+                TrySetErrorResult::NeedDrop => {
+                    drop(err);
+                    return reported_error.id
+                },
+                _ => {}
             }
         }
-    });
-}
-
-pub fn error_handled() {
-    CONTEXTS.with(|contexts| {
-        contexts.borrow_mut().error_count -= 1;
-    });
-}
-
-pub fn error_count() -> u32 {
-    CONTEXTS.with(|contexts| {
-        contexts.borrow().error_count
-    })
-}
-
-pub fn scopes_count() -> usize {
-    CONTEXTS.with(|c| {
-        c.borrow().scopes.len()
+        reported_error.id
     })
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,207 +1,122 @@
+use std::hint::unreachable_unchecked;
+use std::marker::PhantomData;
+use crate::context::PopHandlingContextGuard;
+
 pub mod context;
 
-pub struct PopContextGuard;
+pub trait Error: 'static {}
 
-impl Drop for PopContextGuard {
-    fn drop(&mut self) {
-        unsafe { context::pop_handling_context() }
-    }
+impl<T: 'static> Error for T {}
+
+enum InnerResult<T> {
+    Value(T),
+    Error(u32),
 }
 
 pub struct Result<T> {
-    value: Option<T>,
+    value: InnerResult<T>,
+    _not_send: PhantomData<*mut ()>,
 }
 
 impl<T> Result<T> {
     pub fn is_ok(&self) -> bool {
-        self.value.is_some()
+        matches!(&self.value, InnerResult::Value(_))
     }
 
     pub fn is_error(&self) -> bool {
-        self.value.is_none()
+        !self.is_ok()
     }
 
     pub fn unwrap(self) -> T {
-        self.value.unwrap()
+        match self.value {
+            InnerResult::Value(v) => v,
+            InnerResult::Error(_) => panic!("Unwrapping an error")
+        }
     }
 
-    pub fn report_error<E: 'static>(err: E) -> Self {
-        context::push_error(err);
-        Self { value: None }
+    pub fn error_id(&self) -> Option<u32> {
+        match &self.value {
+            InnerResult::Value(_) => None,
+            InnerResult::Error(id) => Some(*id),
+        }
+    }
+
+    pub unsafe fn unchecked_error_id(&self) -> u32 {
+        match &self.value {
+            InnerResult::Error(x) => *x,
+            _ => unreachable_unchecked(),
+        }
+    }
+
+    pub fn report_error<E: Error>(err: E) -> Self {
+        let id = context::push_error(err);
+        Self { value:  InnerResult::Error(id), _not_send: PhantomData }
     }
 }
 
 impl<T> From<T> for Result<T> {
     fn from(v: T) -> Self {
-        Self { value: Some(v) }
+        Self { value: InnerResult::Value(v), _not_send: PhantomData }
     }
 }
 
-impl<T, E: 'static> From<std::result::Result<T, E>> for Result<T> {
+impl<T, E: Error> From<std::result::Result<T, E>> for Result<T> {
     fn from(val: std::result::Result<T, E>) -> Self {
         match val {
-            Ok(v) => Self { value: Some(v) },
-            Err(e) => Result::report_error(e),
+            Ok(v) => Self { value: InnerResult::Value(v), _not_send: PhantomData },
+            Err(e) => Self::report_error(e),
         }
     }
 }
 
-pub fn try_and_handle<F, FErr, T, HE: 'static>(func: F, err_handler: FErr) -> Result<T>
+fn try_or_handle_one<F, H, T, E>(func: F, handler: H) -> Result<T>
 where
     F: FnOnce() -> Result<T>,
-    FErr: FnOnce(HE) -> Result<T>,
+    H: FnOnce(E) -> Result<T>,
+    E: Error,
 {
-    let mut error_storage: Option<HE> = None;
+    let mut error_storage: Option<(u32, E)> = None;
     unsafe { context::push_handling_context(&mut error_storage) };
-    let guard = PopContextGuard; // Use a guard in case func() panics
+    let pop_guard = PopHandlingContextGuard;
     let res = func();
-    drop(guard);
-    if let Some(err) = error_storage.take() {
-        context::error_handled();
-        return err_handler(err);
+    drop(pop_guard);
+    if let Some(error_id) = res.error_id() {
+        match error_storage {
+            Some((stored_error_id, err)) if stored_error_id == error_id => handler(err),
+            _ => res
+        }
     }
-    return res;
+    else {
+        res
+    }
 }
 
 #[cfg(test)]
-mod tests {
-    use crate::context::error_count;
-
+mod tests
+{
     #[test]
-    fn try_and_handle() {
-        crate::context::reset();
-        enum Err1 {
-            Err,
+    fn try_or_handle_one() {
+        fn func() -> crate::Result<i32> {
+            crate::Result::report_error(true)
         }
 
-        fn err_func() -> crate::Result<i32> {
-            crate::Result::report_error(Err1::Err)
-        }
-
-        fn handler(_e: Err1) -> crate::Result<i32> {
+        let x = crate::try_or_handle_one(func, |_e: bool| {
             1.into()
+        }).unwrap();
+        assert_eq!(x, 1);
+
+        fn two_errors() -> crate::Result<i32> {
+            crate::Result::<i32>::report_error(true);
+            crate::Result::report_error(10)
         }
 
-        let res = crate::try_and_handle(err_func, handler);
-
-        assert_eq!(res.unwrap(), 1);
-        assert_eq!(error_count(), 0);
-    }
-
-    #[test]
-    fn try_and_handle_nested() {
-        crate::context::reset();
-        enum Err1 {
-            Err,
-        }
-
-        fn err_func1() -> crate::Result<i32> {
-            crate::Result::report_error(Err1::Err)
-        }
-
-        fn err_func2() -> crate::Result<i32> {
-            err_func1()
-        }
-
-        fn handler(_: Err1) -> crate::Result<i32> {
-            1.into()
-        }
-
-        let res = crate::try_and_handle(err_func2, handler);
-
-        assert_eq!(res.unwrap(), 1);
-        assert_eq!(error_count(), 0);
-    }
-
-    #[test]
-    fn try_and_handle_nested2() {
-        crate::context::reset();
-        enum Err1 {
-            Err,
-        }
-
-        enum Err2 {
-            Err,
-        }
-
-        fn err_func1() -> crate::Result<i32> {
-            crate::Result::report_error(Err1::Err)
-        }
-
-        fn err_func2() -> crate::Result<i32> {
-            fn my_handler(_: Err2) -> crate::Result<i32> {
-                2.into()
-            }
-            crate::try_and_handle(err_func1, my_handler)
-        }
-
-        fn handler(_: Err1) -> crate::Result<i32> {
-            1.into()
-        }
-
-        let res = crate::try_and_handle(err_func2, handler);
-
-        assert_eq!(res.unwrap(), 1);
-        assert_eq!(error_count(), 0);
-    }
-
-    #[test]
-    fn try_and_handle_nested3() {
-        crate::context::reset();
-        enum Err1 {
-            Err(i32),
-        }
-
-        enum Err2 {
-            Err(i32),
-        }
-
-        fn err_func1() -> crate::Result<i32> {
-            crate::Result::report_error(Err1::Err(2))
-        }
-
-        fn err_func2() -> crate::Result<i32> {
-            fn my_handler(x: Err1) -> crate::Result<i32> {
-                match x {
-                    Err1::Err(x) => crate::Result::report_error(Err2::Err(x)),
-                }
-            }
-            crate::try_and_handle(err_func1, my_handler)
-        }
-
-        let mut handler2_called = false;
-
-        let handler = |e: Err2| {
-            handler2_called = true;
-            match e {
-                Err2::Err(x) => x.into(),
-            }
-        };
-        let res = crate::try_and_handle(err_func2, handler);
-
-        assert_eq!(res.unwrap(), 2);
-        assert_eq!(error_count(), 0);
-    }
-
-    #[test]
-    #[should_panic]
-    fn unhandled_error() {
-        crate::try_and_handle(
-            || -> crate::Result<i32> { crate::Result::report_error(10) },
-            |_: ()| 0.into(),
-        );
-    }
-
-    #[test]
-    fn drop_scope_on_panic() {
-        let x = std::panic::catch_unwind(|| {
-            crate::try_and_handle(
-                || -> crate::Result<i32> { crate::Result::report_error(10) },
-                |_: ()| 0.into(),
-            );
+        let mut called = false;
+        let x = crate::try_or_handle_one(two_errors, |_: bool| {
+            called = true;
+            2.into()
         });
 
-        assert!(x.is_err());
-        assert_eq!(crate::context::scopes_count(), 0);
+        assert!(!called);
+        assert!(x.is_error());
     }
 }
